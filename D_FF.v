92d860f675f1a43b7b86a30448f3639e372e6b02
SANKALPA HOTA <62276478+Sankalpa-Hota@users.noreply.github.com>
2026-02-07 18:10:32 -0800

    Implement async FIFO module with reset
    Added an asynchronous FIFO design module with reset functionality.

--- D_FF.v
+++ D_FF.v
@@ -1,1 +1,15 @@
+//Async FIFO Design
+module ( input clk , rst_n , d ,
+        output reg q);
+  always@ (posedge clk , rst_n)begin
+    if ( !rst_n ) begin
+      q  <= 0;
+    end
+    else begin
+      q <= d;
+    end
+endmodule
 
+// Industry standard is using negedge rst as asynchronous so that they reduce power loss. 1->0 has low power loss compared to 0->1
+
+    
